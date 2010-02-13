44f0eeda413de631ad422955c5409029784bc018
Dirk Eddelbuettel <edd@debian.org>
2010-02-13 03:34:26 +0000

    update copyright pass argc/argv on -- as we take them in main() may as well

--- inst/examples/rinside_sample8.cpp
+++ inst/examples/rinside_sample8.cpp
@@ -2,13 +2,13 @@
 //
 // Simple example showing how to use R[] = ; 
 //
-// Copyright (C) 2009 Dirk Eddelbuettel and GPL'ed 
+// Copyright (C) 2010 Dirk Eddelbuettel and Romain Francois and GPL'ed 
 
 #include "RInside.h"                    // for the embedded R via RInside
 
 int main(int argc, char *argv[]) {
 
-    RInside R;              // create an embedded R instance 
+    RInside R(argc, argv);              // create an embedded R instance 
     
     R["x"] = 10 ;
     R["y"] = 20 ;
