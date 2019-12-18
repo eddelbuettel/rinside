5f58949bd2d89cfe2ded5ec9986c913a5b14efc5
Dirk Eddelbuettel <edd@debian.org>
2019-12-18 10:09:26 -0600

    add variable to exclusion set but do not try quoting tricks

--- src/tools/RInsideEnvVars.r
+++ src/tools/RInsideEnvVars.r
@@ -2,7 +2,8 @@
 #
 # This owes a lot to littler.R  in the littler sources
 
-ExcludeVars <- c("R_SESSION_TMPDIR", "R_HISTFILE", "R_LIBRARY_DIR", "R_LIBS", "R_PACKAGE_DIR")
+ExcludeVars <- c("R_SESSION_TMPDIR", "R_HISTFILE", "R_LIBRARY_DIR", "R_LIBS",
+                 "R_PACKAGE_DIR", "R_SESSION_INITIALIZED")
 IncludeVars <- Sys.getenv()
 IncludeVars <- IncludeVars[grep("^R_",names(IncludeVars),perl=TRUE)]
 if (.Platform$OS.type == "windows") {
@@ -13,6 +14,6 @@ cat("    const char *R_VARS[] = {\n")
 for (i in 1:length(IncludeVars)){
     if (names(IncludeVars)[i] %in% ExcludeVars)
         next
-    cat('        "',names(IncludeVars)[i],'",',shQuote(IncludeVars[i]),',\n',sep='')
+    cat('        "',names(IncludeVars)[i],'","',IncludeVars[i],'",\n',sep='')
 }
 cat("        NULL\n    };\n")
